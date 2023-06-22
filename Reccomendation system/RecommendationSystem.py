f6c81ac36a4ceea337d18e4372cfe5492fe2b584
Nurhat ACAR <108888773+NurhatACAR@users.noreply.github.com>
2023-06-23 01:06:30 +0300

    Update RecommendationSystem.py

--- Reccomendation system/RecommendationSystem.py
+++ Reccomendation system/RecommendationSystem.py
@@ -1,20 +1,20 @@
 import numpy as np
 import pandas as pd
 
-#stun adlarını veriyoruz(csv dosyası değil.)
+#sütun adlarını veriyoruz(csv dosyası değil).
 column_names = ['user_id','item_id','rating','timestamp']
 df = pd.read_csv('users.data', sep='\t', names=column_names)
 
 df.head()
 
-#Kaç Kayıt olduğunu gösterelim
+#Kaç Kayıt olduğunu gösterelim.
 len(df)
 
 #csv uzantılı dosyamızı da ekleyelim.
 movie_titles = pd.read_csv("movie_id_titles.csv")
 movie_titles.head()
 
-#son eklediğimiz dosyanın ekli olduğu kayıtalara bakalım 
+#son eklediğimiz dosyanın ekli olduğu kayıtalara bakalım.
 len(movie_titles)
 
 #iki tabloyu birleştirelim.
@@ -32,42 +32,42 @@ moviemat.head()
 #Contact (1997) filmin değerlerine bakalım.
 
 Contact_user_ratings = moviemat['Contact (1997)']
-Contact_user_ratings.head()#3. kullanıcı 3.0 değer vermiş diğerleri vermemiş(NaN)
+Contact_user_ratings.head()#NaN değerler değerlendirme yapmayan kullanıcılardır.
 
 #korelasyonları hesaplayalım.
 similar_to_Contact = moviemat.corrwith(Contact_user_ratings)
 similar_to_Contact
 
-#NaN Kayıtları silelim
+#NaN Kayıtları silelim.
 corr_Contact = pd.DataFrame(similar_to_Contact, columns=['Correlation'])
 corr_Contact.dropna(inplace=True)
 corr_Contact.head()
 
-#Sıralama yapıp film önerisinebakalım
+#Sıralama yapıp film önerisine bakalım.
 corr_Contact.sort_values('Correlation',ascending=False).head(10)
 
-#alaksız sonuçlar üretebilir nedeni ise oy veren sayısının az olması
-#sağlıklı sonuçlar için 100'den az oy alan sonuçları silelim
+#alaksız sonuçlar üretebilir nedeni ise oy veren sayısının az olması.
+#sağlıklı sonuçlar için 100'den az oy alan sonuçları silelim.
 
 df.head()
 df.drop(['timestamp'], axis=1)
 
-#her filmin ortalama rating değeri için
+#her filmin ortalama rating değeri için.
 ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
 
-#sıralama yapalım
+#sıralama yapalım.
 ratings.sort_values('rating', ascending=False).head()
 
 #her filmin aldığı oy sayısını hesaplayalım.
 ratings['rating_oy_sayisi'] = pd.DataFrame(df.groupby('title')['rating'].count())
 ratings.head()
 
-#şimdi sıralama yapalım
+#şimdi tekrar sıralama yapalım.
 ratings.sort_values('rating_oy_sayisi',ascending = False).head()
 
 corr_Contact.sort_values('Correlation', ascending=False).head(10)
 corr_Contact = corr_Contact.join(ratings['rating_oy_sayisi'])
 corr_Contact.head()
 
-#filtreleme yapıyoruz
+#filtreleme yapıyoruz.
 corr_Contact[corr_Contact['rating_oy_sayisi'] > 100].sort_values('Correlation',ascending=False).head()
